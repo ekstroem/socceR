167b2bc41274b437f29d6758f655f891da4ce9e5
ekstroem <github@ekstroem.com>
2019-02-05 01:23:50 +0100

    Updarted documentation

--- R/collapse_prediction.R
+++ R/collapse_prediction.R
@@ -1,16 +1,22 @@
-#' Convert factor to numeric vector
+#' Create a matrix to collapse tournament predictions to ranks
 #'
-#' Converts the factor labels to numeric values and returns the factor as a numeric vector
+#' Creates a matrix to collapse the rows of a tournamewnt prediction matrix
 #'
 #' Returns a vector of numeric values. Elements in the input factor that cannot be converted to numeric will produce NA.
 #'
-#' @param ranks A factor
-#' @return Returns a numeric vector of the same length as x
+#' @param ranks An integer vector of R ordered elements giving the cut offs of the ranks to create 
+#' @return Returns a numeric matrix with R rows and T columns that can be multiplied on a square prediction matrix to obtain the collapsed predictions
 #' @author Claus Ekstrom \email{ekstrom@@sund.ku.dk}
 #' @keywords manip
 #' @examples
 #'
+#' m2 <- matrix(c(.5, .5, 0, 0, .5, .5, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1), 4)
+#' # Collapse into ranks 1, 2, and 3+4
+#' collapse <- collapse_prediction(c(1, 2, 4))
 #'
+#' collapsed_prediction <- collapse %*% m2
+#' collapsen_prediction
+#' 
 #' @export
 collapse_prediction <- function(ranks=c(1, 2, 3, 4, 8, 16, 32)) {
   m <- matrix(0, ncol=max(ranks), nrow=length(ranks))
